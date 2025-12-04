484f71406e2b00917481c31c415bb7bbe77100eb
Antonin Durand <antonin.durand576@gmail.com>
2025-12-04 09:44:30 +0100

    Fix car function

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -1,7 +1,5 @@
 #include "./../../include/calculator.h"
 
-#include <math.h>
-
 double _add(double a, double b) {
 	return a+b;
 }
@@ -19,5 +17,5 @@ double _div(double a, double b) {
 }
 
 double _car(double a, double b) {
-	return pow(a, b);
+	return a * b;
 }
